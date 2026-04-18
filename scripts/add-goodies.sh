a5b431d36a616125311edd28cb672451f89ce730
riarumoda <justinebudiwijaya@gmail.com>
2026-04-18 22:14:07 +0800

    goodies: add entry for ksunext and ksunext-susfs, set-up susfs before setting up ksu

--- scripts/add-goodies.sh
+++ scripts/add-goodies.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 echo "- Setting up additional goodies..."
 
-# Defualt Exports
+# Default Exports
 export BACKPORT_GENERAL_PATCH="https://github.com/JackA1ltman/NonGKI_Kernel_Build_2nd/raw/refs/heads/mainline/Patches/backport_patches.sh"
-export KSU_SETUP_URI="https://github.com/ReSukiSU/ReSukiSU/raw/refs/heads/main/kernel/setup.sh"
 export BBG_SETUP_URI="https://github.com/vc-teahouse/Baseband-guard/raw/main/setup.sh"
 export SUSFS_PATCH="https://github.com/JackA1ltman/NonGKI_Kernel_Build_2nd/raw/refs/heads/mainline/Patches/Patch/susfs_patch_to_${KERNEL_VERSION}.patch"
 
@@ -11,17 +10,12 @@ export SUSFS_PATCH="https://github.com/JackA1ltman/NonGKI_Kernel_Build_2nd/raw/r
 echo "-- Setting up KernelSU..."
 case "$KERNELSU_SELECTOR" in
     zako|zako-susfs)
-        # Setup KernelSU
-        curl -LSs --fail --retry 3 "$KSU_SETUP_URI" | bash -s dev &> /dev/null || { echo "Fatal: KSU setup script failed to download/run!"; exit 1; }
-        # Enable the necessary KernelSU configs
-        echo "CONFIG_KSU=y" >> $MAIN_DEFCONFIG
-        echo "CONFIG_KSU_MULTI_MANAGER_SUPPORT=y" >> $MAIN_DEFCONFIG
-        echo "CONFIG_KPM=n" >> $MAIN_DEFCONFIG
-        echo "CONFIG_KSU_MANUAL_HOOK=y" >> $MAIN_DEFCONFIG
-        echo "CONFIG_HAVE_SYSCALL_TRACEPOINTS=y" >> $MAIN_DEFCONFIG
-        echo "CONFIG_THREAD_INFO_IN_TASK=y" >> $MAIN_DEFCONFIG
+        # KernelSU Settings
+        export KSU_SETUP_URI="https://github.com/ReSukiSU/ReSukiSU/raw/refs/heads/main/kernel/setup.sh"
+        export KSU_SETUP_BRANCH="main"
         # SUSFS Settings
         if [[ "$KERNELSU_SELECTOR" == "zako-susfs" ]]; then
+            export KSU_SETUP_BRANCH="main"
             wget -qO- $SUSFS_PATCH | patch -s -p1 --fuzz=5
             echo "CONFIG_KSU_SUSFS=y" >> $MAIN_DEFCONFIG
             echo "CONFIG_KSU_SUSFS_SUS_PATH=y" >> $MAIN_DEFCONFIG
@@ -38,6 +32,49 @@ case "$KERNELSU_SELECTOR" in
         else
             KSU_HOOK="https://github.com/JackA1ltman/NonGKI_Kernel_Build_2nd/raw/refs/heads/mainline/Patches/syscall_hook_patches.sh"
         fi
+        # Setup KernelSU
+        curl -LSs --fail --retry 3 "$KSU_SETUP_URI" | bash -s $KSU_SETUP_BRANCH &> /dev/null || { echo "Fatal: KSU setup script failed to download/run!"; exit 1; }
+        # Enable the necessary KernelSU configs
+        echo "CONFIG_KSU=y" >> $MAIN_DEFCONFIG
+        echo "CONFIG_KSU_MULTI_MANAGER_SUPPORT=y" >> $MAIN_DEFCONFIG
+        echo "CONFIG_KPM=n" >> $MAIN_DEFCONFIG
+        echo "CONFIG_KSU_MANUAL_HOOK=y" >> $MAIN_DEFCONFIG
+        echo "CONFIG_HAVE_SYSCALL_TRACEPOINTS=y" >> $MAIN_DEFCONFIG
+        echo "CONFIG_THREAD_INFO_IN_TASK=y" >> $MAIN_DEFCONFIG
+        # Apply backport and hooks
+        curl -LSs "$BACKPORT_GENERAL_PATCH" | bash &> /dev/null
+        curl -LSs "$KSU_HOOK" | bash &> /dev/null
+        ;;
+    ksunext|ksunext-susfs)
+        # KernelSU Settings
+        export KSU_SETUP_URI="https://github.com/KernelSU-Next/KernelSU-Next/raw/refs/heads/dev/kernel/setup.sh"
+        export KSU_SETUP_BRANCH="legacy"
+        # SUSFS Settings
+        if [[ "$KERNELSU_SELECTOR" == "ksunext-susfs" ]]; then
+            export KSU_SETUP_BRANCH="legacy_susfs"
+            wget -qO- $SUSFS_PATCH | patch -s -p1 --fuzz=5
+            echo "CONFIG_KSU_SUSFS=y" >> $MAIN_DEFCONFIG
+            echo "CONFIG_KSU_SUSFS_SUS_PATH=y" >> $MAIN_DEFCONFIG
+            echo "CONFIG_KSU_SUSFS_SUS_MOUNT=y" >> $MAIN_DEFCONFIG
+            echo "CONFIG_KSU_SUSFS_SUS_KSTAT=y" >> $MAIN_DEFCONFIG
+            echo "CONFIG_KSU_SUSFS_SPOOF_UNAME=y" >> $MAIN_DEFCONFIG
+            echo "CONFIG_KSU_SUSFS_ENABLE_LOG=y" >> $MAIN_DEFCONFIG
+            echo "CONFIG_KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS=y" >> $MAIN_DEFCONFIG
+            echo "CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG=y" >> $MAIN_DEFCONFIG
+            echo "CONFIG_KSU_SUSFS_OPEN_REDIRECT=y" >> $MAIN_DEFCONFIG
+            echo "CONFIG_KSU_SUSFS_SUS_MAP=y" >> $MAIN_DEFCONFIG
+            echo "CONFIG_KSU_SUSFS_TRY_UMOUNT=y" >> $MAIN_DEFCONFIG
+            KSU_HOOK="https://github.com/JackA1ltman/NonGKI_Kernel_Build_2nd/raw/refs/heads/mainline/Patches/susfs_inline_hook_patches.sh"
+        else
+            KSU_HOOK="https://github.com/JackA1ltman/NonGKI_Kernel_Build_2nd/raw/refs/heads/mainline/Patches/syscall_hook_patches.sh"
+        fi
+        # Setup KernelSU
+        curl -LSs --fail --retry 3 "$KSU_SETUP_URI" | bash -s $KSU_SETUP_BRANCH &> /dev/null || { echo "Fatal: KSU setup script failed to download/run!"; exit 1; }
+        # Enable the necessary KernelSU configs
+        echo "CONFIG_KSU=y" >> $MAIN_DEFCONFIG
+        echo "CONFIG_KSU_MANUAL_HOOK=y" >> $MAIN_DEFCONFIG
+        echo "CONFIG_HAVE_SYSCALL_TRACEPOINTS=y" >> $MAIN_DEFCONFIG
+        echo "CONFIG_THREAD_INFO_IN_TASK=y" >> $MAIN_DEFCONFIG
         # Apply backport and hooks
         curl -LSs "$BACKPORT_GENERAL_PATCH" | bash &> /dev/null
         curl -LSs "$KSU_HOOK" | bash &> /dev/null
