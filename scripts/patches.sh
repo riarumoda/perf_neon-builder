c6114f90666de263bca3224f961d3d2682f202fa
riarumoda <justinebudiwijaya@gmail.com>
2026-09-21 09:52:31 +0700

    patches: allow -Wno-implicit-int
    Signed-off-by: riarumoda <justinebudiwijaya@gmail.com>

--- scripts/patches.sh
+++ scripts/patches.sh
@@ -482,7 +482,7 @@ esac
 if [[ "$CLANG_STRAT" == "1" ]]; then
     echo "- Variable clang_strat is set to 1! applying extra patches..."
     echo "-- Allowing to compile on new AOSP clang..."
-    sed -i 's/-Wno-format-security/-Wno-format-security -Wno-enum-conversion -Wno-default-const-init-var-unsafe -Wno-default-const-init-field-unsafe -Wno-misleading-indentation -Wno-unsequenced -Wno-sizeof-pointer-memaccess -Wno-implicit-function-declaration -Wno-implicit-enum-enum-cast/g' Makefile
+    sed -i 's/-Wno-format-security/-Wno-format-security -Wno-implicit-int -Wno-enum-conversion -Wno-default-const-init-var-unsafe -Wno-default-const-init-field-unsafe -Wno-misleading-indentation -Wno-unsequenced -Wno-sizeof-pointer-memaccess -Wno-implicit-function-declaration -Wno-implicit-enum-enum-cast/g' Makefile
     echo "-- Setting up -O3 flags..."
     sed -i 's/KBUILD_CFLAGS.*+= -O2/KBUILD_CFLAGS   += -O3/g' Makefile
     if [[ $DEVICE_IMPORT == "spiteful-sweet-aosp-buildout" || $DEVICE_IMPORT == "spiteful-sweet-miui-buildout" ]]; then
