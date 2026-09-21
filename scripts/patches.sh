50874e079396211591eae88f14ca7f580f4f143d
riarumoda <justinebudiwijaya@gmail.com>
2026-09-21 10:17:43 +0700

    patches: touchscreen fix on southwest
    Signed-off-by: riarumoda <justinebudiwijaya@gmail.com>

--- scripts/patches.sh
+++ scripts/patches.sh
@@ -399,6 +399,8 @@ case "$DEVICE_IMPORT" in
         sed -i 's/module_i2c_driver(max98927_i2c_driver)/module_i2c_driver(max98927_i2c_driver);/' techpack/audio/asoc/codecs/max98937.c
         sed -i 's/module_i2c_driver(max98927_i2c_driver);/builtin_i2c_driver(max98927_i2c_driver);/' techpack/audio/asoc/codecs/max98937.c
         sed -i 's/builtin_i2c_driver(max98927_i2c_driver);/static int __init max98927_i2c_init(void) { return i2c_add_driver(\&max98927_i2c_driver); }\nsubsys_initcall(max98927_i2c_init);/' techpack/audio/asoc/codecs/max98937.c
+        echo "-- Fixing touchscreen..."
+        sed -i '/bdata->addr_delay_us/d; /synaptics,address-delay-us/,/}/d' drivers/input/touchscreen/synaptics_dsx_force/synaptics_dsx_spi.c
         enable_erofs
         default_config_fouronenine
     ;;
@@ -409,6 +411,8 @@ case "$DEVICE_IMPORT" in
         sed -i 's/module_i2c_driver(max98927_i2c_driver)/module_i2c_driver(max98927_i2c_driver);/' techpack/audio/asoc/codecs/max98937.c
         sed -i 's/module_i2c_driver(max98927_i2c_driver);/builtin_i2c_driver(max98927_i2c_driver);/' techpack/audio/asoc/codecs/max98937.c
         sed -i 's/builtin_i2c_driver(max98927_i2c_driver);/static int __init max98927_i2c_init(void) { return i2c_add_driver(\&max98927_i2c_driver); }\nsubsys_initcall(max98927_i2c_init);/' techpack/audio/asoc/codecs/max98937.c
+        echo "-- Fixing touchscreen..."
+        sed -i '/bdata->addr_delay_us/d; /synaptics,address-delay-us/,/}/d' drivers/input/touchscreen/synaptics_dsx_force/synaptics_dsx_spi.c
         nethunter_fouronefour_configs
         nethunter_fouronenine_patches
         enable_erofs
