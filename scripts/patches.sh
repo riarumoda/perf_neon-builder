2f8ef472eff05da521c132c4544ee9959875edaa
riarumoda <justinebudiwijaya@gmail.com>
2026-09-21 10:46:58 +0700

    patches: another fix for touchscreen
    Signed-off-by: riarumoda <justinebudiwijaya@gmail.com>

--- scripts/patches.sh
+++ scripts/patches.sh
@@ -400,7 +400,13 @@ case "$DEVICE_IMPORT" in
         sed -i 's/module_i2c_driver(max98927_i2c_driver);/builtin_i2c_driver(max98927_i2c_driver);/' techpack/audio/asoc/codecs/max98937.c
         sed -i 's/builtin_i2c_driver(max98927_i2c_driver);/static int __init max98927_i2c_init(void) { return i2c_add_driver(\&max98927_i2c_driver); }\nsubsys_initcall(max98927_i2c_init);/' techpack/audio/asoc/codecs/max98937.c
         echo "-- Fixing touchscreen..."
-        sed -i '/bdata->addr_delay_us/d; /synaptics,address-delay-us/,/^$/d' drivers/input/touchscreen/synaptics_dsx_force/synaptics_dsx_spi.c
+        echo "-- Fixing touchscreen..."
+        sed -i \
+            -e '/bdata->addr_delay_us/d' \
+            -e '/synaptics,address-delay-us/,/^$/d' \
+            -e '/if (index == 1)/d' \
+            -e '/^\t\t\telse$/d' \
+            drivers/input/touchscreen/synaptics_dsx_force/synaptics_dsx_spi.c
         enable_erofs
         default_config_fouronenine
     ;;
@@ -412,7 +418,12 @@ case "$DEVICE_IMPORT" in
         sed -i 's/module_i2c_driver(max98927_i2c_driver);/builtin_i2c_driver(max98927_i2c_driver);/' techpack/audio/asoc/codecs/max98937.c
         sed -i 's/builtin_i2c_driver(max98927_i2c_driver);/static int __init max98927_i2c_init(void) { return i2c_add_driver(\&max98927_i2c_driver); }\nsubsys_initcall(max98927_i2c_init);/' techpack/audio/asoc/codecs/max98937.c
         echo "-- Fixing touchscreen..."
-        sed -i '/bdata->addr_delay_us/d; /synaptics,address-delay-us/,/^$/d' drivers/input/touchscreen/synaptics_dsx_force/synaptics_dsx_spi.c
+        sed -i \
+            -e '/bdata->addr_delay_us/d' \
+            -e '/synaptics,address-delay-us/,/^$/d' \
+            -e '/if (index == 1)/d' \
+            -e '/^\t\t\telse$/d' \
+            drivers/input/touchscreen/synaptics_dsx_force/synaptics_dsx_spi.c
         nethunter_fouronefour_configs
         nethunter_fouronenine_patches
         enable_erofs
