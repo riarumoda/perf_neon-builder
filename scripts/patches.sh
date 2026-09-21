9268bb0aa0c24a47ad8ec8a80e41b57a8cfdc440
riarumoda <justinebudiwijaya@gmail.com>
2026-09-21 13:46:21 +0700

    devices, patches: remove nethunter-southwest-ng
    Signed-off-by: riarumoda <justinebudiwijaya@gmail.com>

--- scripts/patches.sh
+++ scripts/patches.sh
@@ -419,27 +419,6 @@ case "$DEVICE_IMPORT" in
         enable_erofs
         default_config_fouronenine
     ;;
-    lavender-southwest-ng-nethunter)
-        echo "-- Fixing audio..."
-        sed -i '1i #include <linux/i2c.h>\n#include <linux/module.h>' techpack/audio/asoc/codecs/tas2557_clover/tas2557-regmap.c
-        sed -i '1i #include <linux/i2c.h>\n#include <linux/module.h>' techpack/audio/asoc/codecs/max98937.c
-        sed -i 's/module_i2c_driver(max98927_i2c_driver)/module_i2c_driver(max98927_i2c_driver);/' techpack/audio/asoc/codecs/max98937.c
-        sed -i 's/module_i2c_driver(max98927_i2c_driver);/builtin_i2c_driver(max98927_i2c_driver);/' techpack/audio/asoc/codecs/max98937.c
-        sed -i 's/builtin_i2c_driver(max98927_i2c_driver);/static int __init max98927_i2c_init(void) { return i2c_add_driver(\&max98927_i2c_driver); }\nsubsys_initcall(max98927_i2c_init);/' techpack/audio/asoc/codecs/max98937.c
-        echo "-- Fixing touchscreen..."
-        sed -i \
-            -e '/bdata->addr_delay_us/d' \
-            -e '/synaptics,address-delay-us/,/^$/d' \
-            -e '/if (index == 1)/d' \
-            -e '/^\t\t\telse$/d' \
-            drivers/input/touchscreen/synaptics_dsx_force/synaptics_dsx_spi.c
-        echo "-- Fixing CAN usb..."
-        sed -i '130s/u8 id/unsigned int id/' drivers/net/can/usb/peak_usb/pcan_usb_pro.c
-        nethunter_fouronefour_configs
-        nethunter_fouronenine_patches
-        enable_erofs
-        default_config_fouronenine
-    ;;
     # Spiteful MIUI Buildout
     spiteful-sweet-miui-buildout)
         echo "-- Reverting hard to commits before KSU is being added..."
