d3b2a626a917107c2ba773f935e1a8ae90f97eff
riarumoda <justinebudiwijaya@gmail.com>
2026-09-21 10:04:31 +0700

    patches: another ducttape for max98937
    Signed-off-by: riarumoda <justinebudiwijaya@gmail.com>

--- scripts/patches.sh
+++ scripts/patches.sh
@@ -398,6 +398,7 @@ case "$DEVICE_IMPORT" in
         sed -i '1i #include <linux/i2c.h>\n#include <linux/module.h>' techpack/audio/asoc/codecs/max98937.c
         sed -i 's/module_i2c_driver(max98927_i2c_driver)/module_i2c_driver(max98927_i2c_driver);/' techpack/audio/asoc/codecs/max98937.c
         sed -i 's/module_i2c_driver(max98927_i2c_driver);/builtin_i2c_driver(max98927_i2c_driver);/' techpack/audio/asoc/codecs/max98937.c
+        sed -i 's/builtin_i2c_driver(max98927_i2c_driver);/static int __init max98927_i2c_init(void) { return i2c_add_driver(\&max98927_i2c_driver); }\nsubsys_initcall(max98927_i2c_init);/' techpack/audio/asoc/codecs/max98937.c
         enable_erofs
         default_config_fouronenine
     ;;
@@ -407,6 +408,7 @@ case "$DEVICE_IMPORT" in
         sed -i '1i #include <linux/i2c.h>\n#include <linux/module.h>' techpack/audio/asoc/codecs/max98937.c
         sed -i 's/module_i2c_driver(max98927_i2c_driver)/module_i2c_driver(max98927_i2c_driver);/' techpack/audio/asoc/codecs/max98937.c
         sed -i 's/module_i2c_driver(max98927_i2c_driver);/builtin_i2c_driver(max98927_i2c_driver);/' techpack/audio/asoc/codecs/max98937.c
+        sed -i 's/builtin_i2c_driver(max98927_i2c_driver);/static int __init max98927_i2c_init(void) { return i2c_add_driver(\&max98927_i2c_driver); }\nsubsys_initcall(max98927_i2c_init);/' techpack/audio/asoc/codecs/max98937.c
         nethunter_fouronefour_configs
         nethunter_fouronenine_patches
         enable_erofs
